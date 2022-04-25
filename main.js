5fcc58e12e4f7883cc60a9af0222161eb478f9de
Arturgouveia1970 <artgouveiag@gmail.com>
2022-04-25 16:15:23 +0100

    addeddinamic data

--- main.js
+++ main.js
@@ -12,3 +12,25 @@ const submitHandler = (e) => {
   library.push(singleBook);
   window.localStorage.setItem('singleBook', JSON.stringify(library));
 };
+
+window.onload = () => {
+  const formData = document.getElementById('new-book');
+  const bookList = document.getElementById('book-list');
+  formData.addEventListener('submit', submitHandler);
+  const libraryData = window.JSON.parse(localStorage.getItem('singleBook') || '[]');
+  bookList.innerHTML = libraryData.map((el, index) => ` <div>
+       <h4>${el.title}</h4>
+       <p>${el.author}</p>
+       <button type="button" data-id=${index} class="remove-button">Remove</button>
+       </div>`);
+  const removeBtn = document.querySelectorAll('.remove-button');
+  removeBtn.forEach((el) => {
+    el.addEventListener('click', (event) => {
+      const dataToFilter = event.target.dataset.id;
+      library.splice(dataToFilter);
+      // localStorage.setItem('questions',JSON.stringify(questions));
+      console.log(libraryData);
+    });
+  });
+};
+
