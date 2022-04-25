3c6dd8de1d842d9d61a5fd974e4dbf32e63a1fa9
bizip <bizip04@gmail.com>
2022-04-25 17:57:13 +0200

    Add remove book functionalities

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ const submitHandler = (e) => {
 
   library.push(singleBook);
   window.localStorage.setItem('singleBook', JSON.stringify(library));
+  const formData = document.getElementById('new-book');
+  formData.reset();
 };
 
 window.onload = () => {
@@ -18,18 +20,21 @@ window.onload = () => {
   const bookList = document.getElementById('book-list');
   formData.addEventListener('submit', submitHandler);
   const libraryData = window.JSON.parse(localStorage.getItem('singleBook') || '[]');
-  bookList.innerHTML = libraryData.map((el, index) => ` <div>
-       <h4>${el.title}</h4>
-       <p>${el.author}</p>
-       <button type="button" data-id=${index} class="remove-button">Remove</button>
-       </div>`);
-  const removeBtn = document.querySelectorAll('.remove-button');
-  removeBtn.forEach((el) => {
-    el.addEventListener('click', (event) => {
-      const dataToFilter = event.target.dataset.id;
-      library.splice(dataToFilter);
-      // localStorage.setItem('questions',JSON.stringify(questions));
-      console.log(libraryData);
+  if (libraryData.length < 1) {
+    bookList.innerHTML = '<h1>There is no awesome books in library!</h1><br />';
+  } else {
+    bookList.innerHTML = libraryData.map((el, index) => ` <div>
+        <h4>${el.title}</h4>
+        <p>${el.author}</p>
+        <button type="button" data-id=${index} class="remove-button">Remove</button>
+        </div>`);
+    const removeBtn = document.querySelectorAll('.remove-button');
+    removeBtn.forEach((el) => {
+      el.addEventListener('click', (event) => {
+        const dataToFilter = event.target.dataset.id;
+        libraryData.splice(dataToFilter, 1);
+        window.localStorage.setItem('singleBook', JSON.stringify(libraryData));
+      });
     });
-  });
+  }
 };
